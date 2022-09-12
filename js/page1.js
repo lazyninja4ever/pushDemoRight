1bc52fb23f4e8c11af2c3e76701123a0242b4e1c
Your Name <you@example.com>
2022-09-12 12:33:07 +0200

    page

--- js/page1.js
+++ js/page1.js
@@ -1,3 +1,11 @@
 let nptFld = document.getElementById('usrNpt');
 let btn = document.getElementById('addBtn');
 let listOut = document.getElementById('list');
+
+const addToList = () => {
+  let newLi = document.createElement('p');
+  newLi.innerHTML = nptFld.value;
+  listOut.append(newLi);
+}
+
+btn.addEventListener('click', addToList);
