c532f7c92c9b0a6dd89798d902ebdd1bb20acc8f
Your Name <you@example.com>
2022-09-12 13:15:03 +0200

    page3

---
+++ js/page3.js
@@ -0,0 +1,26 @@
+const numb =Math.floor(Math.random()*1000);
+btn = document.getElementById('checkNumb');
+inputEl = document.getElementById('usrIpt');
+output = document.getElementById('output');
+let count = 0;
+
+const checkNumber = () => {
+  input = Number(inputEl.value);
+  count++;
+  input === numb ? right() : wrong ();
+
+}
+const wrong = () => {
+  let msg;
+  input < numb ? msg=`higher than ${input}` : msg =`lower than ${input}`;
+  output.innerHTML = msg;
+}
+
+const right = () => {
+  output.innerHTML = `gættet rigtigt på ${count} forsøg`;
+
+}
+
+btn.addEventListener('click', checkNumber);
+
+
