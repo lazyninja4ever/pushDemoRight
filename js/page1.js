bd2fc0404bb0dcb24a6e001b797afb1733f98899
Your Name <you@example.com>
2022-09-13 10:44:28 +0200

    added log

--- js/page1.js
+++ js/page1.js
@@ -2,6 +2,8 @@ let nptFld = document.getElementById('usrNpt');
 let btn = document.getElementById('addBtn');
 let listOut = document.getElementById('list');
 
+console.log('hej');
+
 const addToList = () => {
   let newLi = document.createElement('p');
   newLi.innerHTML = nptFld.value;
